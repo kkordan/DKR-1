f17b8733349a67f0dfccddcfd349e70bb5affddd
kkordan <94549547+kkordan@users.noreply.github.com>
2022-11-03 09:30:21 +0300

    Add files via upload

---
+++ ДКР1(1).pas
@@ -0,0 +1,17 @@
+﻿var x,y:real;
+begin
+  write('введите x: ');
+  readln(x);
+  if x<-8 then
+    y:=power(x,1/3)
+  else 
+    if x<1 then 
+    y:=log(x)*x+exp(x)/sin(x)
+    else
+      if x<4 then
+      y:=ln(x)/power(x,(0.1*x))
+      else
+        y:=ln(x)/82-x;
+      write('y= ',y:2:5);
+end.
+  
---
+++ ДКР1(2).pas
@@ -0,0 +1,28 @@
+﻿var y,x,b,h:real;
+begin
+ 
+  write('введите интервал: ');
+  readln(x,b);
+  write('введите шаг: ');
+  readln(h);
+  while x<=b do
+  begin
+  if x<-8 then
+    y:=power(x,1/3)
+  else 
+    if x<1 then 
+    y:=log(x)*x+exp(x)/sin(x)
+    else
+      if x<4 then
+      y:=ln(x)/power(x,(0.1*x))
+      else
+        begin
+        y:=ln(x)/82-x;
+      end;
+      
+      writeln('x=',x,' y=',y:2:6);
+      x:=x+h;
+      end;
+      
+end.
+  
